a2df38b1e08bcf0b6b8c12dfa39b6ce08399329d
motiurrahmanmisu <motiurrahmanmisu@gmail.com>
2021-09-26 20:27:05 +0600

    actors component change

--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -13,7 +13,7 @@ const Member = (props) => {
     return (
         <div className="bg-light rounded p-2 mt-3 ms-2">
             {/* total added */}
-            <h4 style={{ color: "rgb(116, 0, 1)" }} className="header-title"><i className="fas fa-users" style={{ color: "rgb(116, 0, 1)" }}></i> Total Added: {props.member.length}</h4>
+            <h4 style={{ color: "rgb(116, 0, 1)" }} className="header-title"><i className="fas fa-users" style={{ color: "rgb(116, 0, 1)" }}></i> Actors Added: {props.member.length}</h4>
             {/* total cost */}
             <h5 className="ms-4 header-title" style={{ color: "rgb(116, 0, 1)" }}>Total Cost: ${total}</h5>
             {/* added name */}
